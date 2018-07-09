d94f79837dabbdf3eb6375dec646b94a2f523c1e
RyanB02 <ryanbpy1@gmail.com>
2018-07-09 18:12:54 +0100

    -

--- resources/views/backup - 5-04-2018/common/user_log_err.blade.php
+++
@@ -1,8 +0,0 @@
-
-<div class="card card-default  mb-4" style="border-color: #CF2941; ">
-	<div class="card-header bg-danger">Authentication Error - Don't try that again! </div>
-	<div class="card-body" style="font-size: 20px">
-		IP: <?php echo $_SERVER['REMOTE_ADDR'];?> <br> 
-		If this IP is used again to try to bypass our authentication, the ip will be blocked from our site!
-	</div>
-</div>
--- resources/views/layouts/partials/special.blade.php
+++
@@ -1,9 +0,0 @@
-        <div class="top_desktop">
-            <div class="container justify-content-end" style="padding: 0px !important; margin-top: 0px !important">
-
-               <h1 style="font-size: 20px; font-family: Montserrat; text-align: ; margin-top: 5px !important; padding: 0px !important; margin:0px;"> <a href="/" style=" text-decoration: none; color: inherit;">Saddleworth Literary Festival</a></h1>
-            </div>
-
-            
-        </div>
-        <hr style="border-color: white; margin-top: 0px; padding: 0px">
